5a103e8cd3a5024edcfcafdceadfdca54e63845e
fibrous <65938238+fibrous-sausage@users.noreply.github.com>
2025-02-07 12:17:10 -0500

    Implement ability to color grid by hovering over squares

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ for (let i = 0; i < 16 * 16; ++i) {
     cell.classList.add("cell");
     container.appendChild(cell);
 }
+
+container.addEventListener("mouseover", (event) => {
+    let cell = event.target;
+    cell.style.backgroundColor = "black";
+});
